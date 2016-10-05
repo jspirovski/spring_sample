1bdac2f835f52fdf078fa2c937d65efe828b4c48
Jovica Spirovski <jspirovski@endava.com>
2016-10-05 13:03:29 +0200

    first commit

---
+++ src/com/endava/model/Customer.java
@@ -0,0 +1,28 @@
+package com.endava.model;
+
+/**
+ * Created by jspirovski on 10/5/2016.
+ */
+public class Customer {
+
+    private String firstName;
+    private String lastName;
+
+    public Customer() {}
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/com/endava/repository/HibernateCustomerRepositoryImpl.java
@@ -0,0 +1,7 @@
+package com.endava.repository;
+
+/**
+ * Created by jspirovski on 10/5/2016.
+ */
+public class HibernateCustomerRepositoryImpl {
+}
